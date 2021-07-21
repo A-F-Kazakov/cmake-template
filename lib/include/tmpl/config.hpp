eb2b8073734c2a2605976d9bdb633b4362f5122c
Андрей Казаков <863955c1@mail.ru>
2021-07-21 21:00:09 +0000

    update of documentation

---
+++ lib/include/tmpl/config.hpp
@@ -0,0 +1,14 @@
+#ifndef TMPL_LIB_CONFIG_HPP
+#define TMPL_LIB_CONFIG_HPP
+
+#define LIB_NAMESPACE tmpl::lib
+
+namespace LIB_NAMESPACE
+{
+	constexpr auto version = "0.0.1";
+	constexpr auto version_major{0};
+	constexpr auto version_minor{0};
+	constexpr auto version_patch{1};
+}
+
+#endif
